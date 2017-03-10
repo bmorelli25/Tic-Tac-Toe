cdf6060e94feafb8ae644d09da0b60195a0c8622
Brandon Morelli <bmorelli25@gmail.com>
2017-03-10 13:43:59 -0800

    App.js basic fuctionality

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Announcement from './Announcement';
+import ResetButton from './ResetButton';
+import Tile from './Tile'
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gameBoard: [
+        ' ',' ',' ',
+        ' ',' ',' ',
+        ' ',' ',' '
+      ]
+    }
+  }
   render() {
     return (
-
+      <div className="container">
+        <div className="menu">
+          <h1>Tic-Tac-Toe</h1>
+          <Announcement />
+          <ResetButton />
+        </div>
+        {this.state.gameBoard.map(function(value, i){
+          <Tile />
+        })};
+      </div>
     );
   }
 }
